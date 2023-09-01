d2aec3732a5208adf67fc151df77228c21f462fe
cosmopolitan0713 <135200958+cosmopolitan0713@users.noreply.github.com>
2023-09-01 19:12:02 +0900

    Add files via upload

---
+++ React-Sidebar/src/components/SidebarIcon.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Icon from '../images/season_character3_autumn_momiji.png'
+
+function SidebarIcon() {
+  return (
+    <div className="SidebarIcon">
+      <img src={Icon}  />
+      <p>programing@gmail.com</p>
+
+    </div>
+  )
+}
+
+export default SidebarIcon; 
