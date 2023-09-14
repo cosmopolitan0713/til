9420c252b526c1d8216a9b8e51d310f6d28f796c
cosmopolitan0713 <135200958+cosmopolitan0713@users.noreply.github.com>
2023-09-15 04:24:52 +0900

    Add files via upload

---
+++ React-Three.js-Fiber/src/App.js
@@ -0,0 +1,54 @@
+import { useRef, useState } from 'react';
+import './App.css';
+import { Canvas, useFrame } from "@react-three/fiber"
+import { config, useSpring, animated } from "@react-spring/three"
+
+function Box(props) {
+  const ref = useRef();
+  const [clicked, setClicked] = useState(false);
+  const [hovered, setHovered] = useState(false);
+
+  useFrame(() => (ref.current.rotation.x += 0.01));
+
+  const { scale } = useSpring({
+    scale: clicked ? 2 : 1,
+    config: config.wobbly,
+  })
+
+
+  return (
+    <animated.mesh
+      {...props} ref={ref}
+      onClick={() =>
+        setClicked(!clicked)}
+      onPointerOver={() => setHovered(true)}
+      onPointerOut={() => setHovered(false)}
+      scale={scale}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "red" : "orange"} />
+    </animated.mesh >
+  );
+}
+
+function App() {
+  return (
+    <>
+      <div id="canvas-container">
+        <Canvas>
+          <Box position={[-1.6, 0, 0]} />
+          <Box position={[1.6, 0, 0]} />
+          <Box position={[0, 0, 0]} />
+          <Box position={[-3.2, 0, 0]} />
+          <Box position={[3.2, 0, 0]} />
+          <ambientLight intensity={5} />
+          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+          <pointLight position={[-10, -10, -10]} />
+        </Canvas>
+      </div >
+      <h1>Threejs Fiber</h1>
+      <a href="">もっと見る</a>
+    </>
+  );
+}
+
+export default App;
